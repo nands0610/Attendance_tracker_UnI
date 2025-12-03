43fd7da63daced0561b81bf812f9d88cfb3e959d
Nanditha <sn06102005@gmail.com>
2025-12-03 15:55:40 +0530

    Database connection

--- scraper/try.js
+++ scraper/try.js
@@ -39,18 +39,37 @@ async function fetchData() {
       },
     });
 
+    function extractDate(record) {
+      const raw = record.fields["Walkthrough ID"];  // e.g. "Name - BLM, 12/10/25"
+      const datePart = raw.split(",")[1].trim();    // "12/10/25"
+
+      const [day, month, year] = datePart.split("/");
+      const fullYear = "20" + year;
+
+      return `${fullYear}-${month}-${day}`;
+    }
+
+    const finalData = records.map(record => ({
+      date: extractDate(record),
+      lc_leader_name: record.fields["LC Leader Name"],
+      attendance_type: record.fields["Attendance Type"],
+      duration: record.fields["Duration"]
+    }));
+
     console.log("Status:", response.status);
     console.log("Data:", response.data);
 
     const records = response.data.records;
 
-    records.forEach((record, index) => {
-      console.log(`Record ${index + 1}:`, record.fields);
-    });
+    console.log(finalData)
 
-    records.forEach((record, index) => {
-      console.log(`Record ${index + 1}:`, record.fields['Walkthrough ID']);
-    });
+    // records.forEach((record, index) => {
+    //   console.log(`Record ${index + 1}:`, record.fields);
+    // });
+
+    // records.forEach((record, index) => {
+    //   console.log(`Record ${index + 1}:`, record.fields["Duration"]);
+    // });
 
 
   } catch (error) {
